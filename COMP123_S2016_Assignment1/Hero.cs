ba02b94d016c9be61353176d44faac28eb1c7d10
gapillan <gpaillan@my.centennialcollege.ca>
2016-06-02 18:59:21 -0700

    Added private instance variables: name, strength, speed and health

--- COMP123_S2016_Assignment1/Hero.cs
+++ COMP123_S2016_Assignment1/Hero.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace COMP123_S2016_Assignment1
 {
+    /**
+     * <summary>
+     * This class defines a Hero with special abilities  
+     * </summary> 
+     * 
+     *@class Hero 
+     */
+
     class Hero
     {
+        // ++++++++++++++++++++++ PRIVATE INSTANCE VARIABLES +++++++++++++++++++++++++++
+        private string _name;
+        private int _strength;
+        private int _speed;
+        private int _health; 
+
+        // ++++++++++++++++++++++ PUBLIC PROPERTIES ++++++++++++++++++++++++++++++++++++
+
+        // ++++++++++++++++++++++ CONSTRUCTORS +++++++++++++++++++++++++++++++++++++++++
+
+        // ++++++++++++++++++++++ PUBLIC METHODS +++++++++++++++++++++++++++++++++++++++
     }
 }
